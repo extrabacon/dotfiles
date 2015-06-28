8c55fa9a84cd57cbd27a8c88918a2efcbb8c5db0
Nicolas <Nic@MacBookPro.local>
2015-06-28 14:20:01 -0400

    revised install script

--- init/install.sh
+++ init/install.sh
@@ -5,51 +5,50 @@ cp .* ~/
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install formulas
-brew install \
-brew-cask \
-imagemagick \
-git \
-heroku-toolbelt \
-mongodb \
-node.js \
-flow \
-watchman
+brew install brew-cask
+brew install imagemagick --with-webp
+brew install git
+brew install heroku-toolbelt
+brew install mongodb
+brew install node.js
+brew install flow
+brew install watchman
+brew install rename
+brew install speedtest-cli
+brew install tree
 
 # Install apps
-brew cask install \
-alfred \
-atom \
-dropbox \
-firefox \
-github \
-google-drive \
-rdio \
-skype \
-spectacle \
-vlc
+brew cask install alfred
+brew cask install atom
+brew cask install dropbox
+brew cask install firefox
+brew cask install github
+brew cask install google-drive
+brew cask install rdio
+brew cask install skype
+brew cask install spectacle
+brew cask install vlc
 
 # Install node packages
-npm install -g \
-bower \
-eslint \
-eslint-plugin-react \
-istanbul \
-mocha \
-n \
-nightwatch \
-node-inspector \
-nodemon
+npm install -g bower
+npm install -g eslint
+npm install -g eslint-plugin-react
+npm install -g istanbul
+npm install -g mocha
+npm install -g n
+npm install -g nightwatch
+npm install -g node-inspector
+npm install -g nodemon
 
 # Extensions
-brew cask install \
-qlcolorcode \
-qlstephen \
-qlmarkdown \
-quicklook-json \
-quicklook-csv \
-betterzipql \
-qlimagesize \
-webpquicklook \
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install qlimagesize
+brew cask install webpquicklook
 
 # Create links
 ln -s /usr/local/Cellar ~/Dev/Cellar
