c4b87569d24fb23183155301e66b3dab54d234b8
Nicolas <Nic@MacBookPro.local>
2015-06-27 21:02:43 -0400

    initial customizations

---
+++ init/install_apps.sh
@@ -0,0 +1,32 @@
+brew install \
+brew-cask \
+imagemagick \
+git \
+heroku-toolbelt \
+mongodb \
+node.js \
+flow \
+watchman
+
+brew cask install \
+alfred \
+atom \
+dropbox \
+firefox \
+github \
+google-drive \
+rdio \
+skype \
+spectacle \
+vlc
+
+npm install -g \
+bower \
+eslint \
+eslint-plugin-react \
+istanbul \
+mocha \
+n \
+nightwatch \
+node-inspector \
+nodemon
