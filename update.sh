34f0c6d7b68dd1e8cbc2ce9429e8f1a84370bf6f
Nicolas Mercier <nicolas@extrabacon.net>
2016-02-04 09:15:01 -0500

    new update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+softwareupdate -i -a
+
+brew update
+brew upgrade --all
+brew cleanup
+
+npm update npm -g
+npm update -g
+
+apm upgrade --no-confirm
+
+gem update --system
+gem update
